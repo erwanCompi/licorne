d0654937b76a6d1b5212945e2f41c4bc2535938e
r4z0rFr <erwankreutz@gmail.com>
2020-11-17 15:49:23 +0100

    ✨ Add login buttons

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@
         <div class="w-full max-w-xs">
             <form class="flex bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <button class="flex w-full mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline" type="button">
-                    <img class="w-6" src="https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_28dp.png" alt="firebase logo">
-                    <span>Firebase</span>
+                    <img class="w-6 h-6" src="https://www.gstatic.com/mobilesdk/160503_mobilesdk/logo/2x/firebase_28dp.png" alt="firebase logo">
+                    <span class="ml-1">Firebase</span>
                 </button>
-                <button class="flex w-full mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                    Amplify
+                <button class="flex w-full mx-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline" type="button">
+                    <img class="w-6 h-6" src="https://raw.githubusercontent.com/aws-amplify/aws-amplify.github.io/master/images/Logos/Amplify%20Logo.svg" alt="">
+                    <span class="ml-1">Amplify</span>
                 </button>
             </form>
             <p class="text-center text-gray-500 text-xs">
